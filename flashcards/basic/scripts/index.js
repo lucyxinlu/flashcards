b38ca5b7a7366d2fb11b00aa64b3f5e08a84555c
lucyxinlu <84947231+lucyxinlu@users.noreply.github.com>
2021-06-30 12:51:57 +0800

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -7,7 +7,7 @@ this.correct=false;}
 let myFlashcards=[];
 myFlashcards.push(new question('What is the capital of China?','Beijing'));
 myFlashcards.push(new question('Who is the president of USA?','Biden'));
-myFlashcards.push(new question('What is the name your university?','CMU'));
+myFlashcards.push(new question('What is the name of your university?','CMU'));
 let i=0;
 while (i<myFlashcards.length){
 console.log(myFlashcards[i].question);
