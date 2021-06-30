951a01c02194b940ad46b54df50fe4d5374bf6c0
lucyxinlu <84947231+lucyxinlu@users.noreply.github.com>
2021-06-30 11:40:27 +0800

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,1 +1,16 @@
+class question{
+constructor(question,answer){
+this.question=question;
+this.answer=answer;
+this.correct=false;}
+}
+let myFlashcards=[];
+myFlashcards.push(new question('What is the capital of China?','Beijing'));
+myFlashcards.push(new question('Who is the president of USA?','Biden'));
+myFlashcards.push(new question('What is the name your university?','CMU'));
+let i=0;
+while (i<myFlashcards.length){
+console.log(myFlashcards[i].question);
+  ++i;
+}
 
